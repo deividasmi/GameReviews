9ce1f82333b2f67c6473209faef9a2e2f0dd264c
juoz <d.j.miliauskas@gmail.com>
2019-05-04 18:18:35 +0300

    user profile update

--- Users/forms.py
+++ Users/forms.py
@@ -3,9 +3,19 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
+
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
--- Users/views.py
+++ Users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -20,4 +20,16 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'Users/profile.html')
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(request, f'Account updated succesfully.')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+
+    context = {
+        'u_form' : user_form
+    }
+    return render(request, 'Users/profile.html', context)
